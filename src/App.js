a00ea7486314802af5c18908f62f1525352ba0eb
Victor Huggo <victorhuggo97@gmail.com>
2020-10-17 15:05:40 -0300

    Ajustes no css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { useEffect, AllHTMLAttributes } from 'react';
+import React, { useEffect } from 'react';
 import logo from './images/logo.png';
 import './App.css';
 
 function App() {
   
   useEffect(() => {
-    interface Header extends AllHTMLAttributes<HTMLHeadingElement>{
-      label: string;
-    }
-    // Atualiza o titulo do documento usando a API do browser
     window.onscroll = function () {
       "use strict";
-      let header:Header<> = document.querySelector("header");
+      let header = document.querySelector("header");
       if (window.pageYOffset > 50) {
         header.style.background = "#2A0052";
       } else {
@@ -26,7 +22,7 @@ function App() {
       <header id="header">
         <nav>
           <ul>
-            <li><a href="#">Home</a></li>
+            <li><a className="selected" href="#">Home</a></li>
             <li><a href="#">What we do?</a></li>
             <li><a href="#">Testimonial</a></li>
             <li><a href="#">Contact Us</a></li>
@@ -45,15 +41,14 @@ function App() {
             <h1>Art is Eternal Happiness</h1>
             <a href="#" className="cta">WORK WITH US</a>
           </div>
+            <button className="scroll">Scroll down</button>
         </main>
       </section>
       <section id="content">
-        <main>
           <div>
             <h1>Art is Eternal Happiness</h1>
             <a href="#" className="cta">WORK WITH US</a>
           </div>
-        </main>
       </section>
     </div>
   );
