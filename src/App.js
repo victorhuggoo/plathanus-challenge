9fb825c966ef764c4e90a1c387a5373046dcbf39
Victor Huggo <victorhuggo97@gmail.com>
2020-10-19 21:58:32 -0300

    apicando detalhes ao front end

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { useEffect } from 'react';
 import logo from './images/logo.png';
+import { FaCameraRetro, FaTachometerAlt, FaBriefcase, FaSearch, FaArrowDown } from 'react-icons/fa'
 import './App.css';
 
 function App() {
-  
+
   useEffect(() => {
+    var links = document.querySelector('.link')
+    links.addEventListener('click', () => {
+      links.classList.remove('selected')
+      this.classList.add('selected')
+    })
+
+    console.log(links);
+    
+
     window.onscroll = function () {
       "use strict";
       let header = document.querySelector("header");
+      let search = document.querySelector(".search-box");
       if (window.pageYOffset > 50) {
         header.style.background = "#2A0052";
+        search.style.background ="#FFFF"
       } else {
         header.style.background = "transparent";
+        search.style.background ="rgba(255,255,255,0.3)"
+
       }
     }
-  },[]);
+  }, []);
 
   return (
 
@@ -22,10 +36,10 @@ function App() {
       <header id="header">
         <nav>
           <ul>
-            <li><a className="selected" href="#">Home</a></li>
-            <li><a href="#">What we do?</a></li>
-            <li><a href="#">Testimonial</a></li>
-            <li><a href="#">Contact Us</a></li>
+            <li><a className="link selected" href="#">Home</a></li>
+            <li><a className="link" href="#whatwedo">What we do?</a></li>
+            <li><a href="javascript:void(0)">Testimonial</a></li>
+            <li><a href="javascript:void(0)">Contact Us</a></li>
           </ul>
         </nav>
         <div className="logo">
@@ -33,21 +47,43 @@ function App() {
         </div>
         <div className="search-box">
           <input type="text" name="search" id="search" placeholder="Search..." />
+          <FaSearch/>
         </div>
       </header>
-      <section id="slide">
+      <section>
         <main>
           <div>
             <h1>Art is Eternal Happiness</h1>
             <a href="#" className="cta">WORK WITH US</a>
           </div>
-            <button className="scroll">Scroll down</button>
+          <a href="#whatwedo" className="scroll">Scroll down  <FaArrowDown/></a>
         </main>
       </section>
-      <section id="content">
-          <div>
-            <h1>Art is Eternal Happiness</h1>
-            <a href="#" className="cta">WORK WITH US</a>
+      <section id="whatwedo">
+        <div id="title">
+            <h1>What we do</h1>
+            <hr/>
+            <p>Vivamus purus lorem, facilisis id nibh eget, facilisis gravida mauris. Maecenas eget volutpat neque</p>
+        </div>
+          <div id="content">
+            <div id="firstItem">
+              <FaCameraRetro className='icon'/>
+              <h2>Lorem Ipsum</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
+              <a href="#">Learn more</a>
+            </div>
+            <div id="secondItem">
+            <FaTachometerAlt className='icon'/>
+              <h2>Lorem Ipsum</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
+              <a href="#">Learn more</a>
+            </div>
+            <div id="thridItem">
+            <FaBriefcase className='icon'/>
+              <h2>Lorem Ipsum</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
+              <a href="#">Learn more</a>
+            </div>
           </div>
       </section>
     </div>
