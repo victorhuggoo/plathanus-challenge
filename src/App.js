c208b24ef6ae8b4c2709cab610bbccd96879c66a
Victor Huggo <victorhuggo97@gmail.com>
2020-10-20 07:37:50 -0300

    Criando modal para upload de imagem

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import './App.css';
 function App() {
 
   useEffect(() => {
-    var links = document.querySelector('.link')
-    links.addEventListener('click', () => {
-      links.classList.remove('selected')
-      this.classList.add('selected')
-    })
+    // var links = document.querySelector('.link')
 
-    console.log(links);
-    
+    // links.addEventListener('click', () => {
+    //   links.classList.remove('selected')
+    //   console.log(links);
+    // })
+
+    // console.log(links);
+
 
     window.onscroll = function () {
       "use strict";
@@ -21,10 +22,10 @@ function App() {
       let search = document.querySelector(".search-box");
       if (window.pageYOffset > 50) {
         header.style.background = "#2A0052";
-        search.style.background ="#FFFF"
+        search.style.background = "#FFFF"
       } else {
         header.style.background = "transparent";
-        search.style.background ="rgba(255,255,255,0.3)"
+        search.style.background = "rgba(255,255,255,0.3)"
 
       }
     }
@@ -36,10 +37,10 @@ function App() {
       <header id="header">
         <nav>
           <ul>
-            <li><a className="link selected" href="#">Home</a></li>
-            <li><a className="link" href="#whatwedo">What we do?</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#whatwedo">What we do?</a></li>
             <li><a href="javascript:void(0)">Testimonial</a></li>
-            <li><a href="javascript:void(0)">Contact Us</a></li>
+            <li><a href="#abrirModal">Config</a></li>
           </ul>
         </nav>
         <div className="logo">
@@ -47,7 +48,7 @@ function App() {
         </div>
         <div className="search-box">
           <input type="text" name="search" id="search" placeholder="Search..." />
-          <FaSearch/>
+          <FaSearch />
         </div>
       </header>
       <section>
@@ -56,35 +57,45 @@ function App() {
             <h1>Art is Eternal Happiness</h1>
             <a href="#" className="cta">WORK WITH US</a>
           </div>
-          <a href="#whatwedo" className="scroll">Scroll down  <FaArrowDown/></a>
+          <a href="#whatwedo" className="scroll">Scroll down  <FaArrowDown /></a>
         </main>
       </section>
       <section id="whatwedo">
         <div id="title">
-            <h1>What we do</h1>
-            <hr/>
-            <p>Vivamus purus lorem, facilisis id nibh eget, facilisis gravida mauris. Maecenas eget volutpat neque</p>
+          <h1>What we do</h1>
+          <hr />
+          <p>Vivamus purus lorem, facilisis id nibh eget, facilisis gravida mauris. Maecenas eget volutpat neque</p>
         </div>
-          <div id="content">
-            <div id="firstItem">
-              <FaCameraRetro className='icon'/>
-              <h2>Lorem Ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
-              <a href="#">Learn more</a>
-            </div>
-            <div id="secondItem">
-            <FaTachometerAlt className='icon'/>
-              <h2>Lorem Ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
-              <a href="#">Learn more</a>
-            </div>
-            <div id="thridItem">
-            <FaBriefcase className='icon'/>
-              <h2>Lorem Ipsum</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
-              <a href="#">Learn more</a>
-            </div>
+        <div id="content">
+          <div id="firstItem">
+            <FaCameraRetro className='icon' />
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
+            <a href="#">Learn more</a>
           </div>
+          <div id="secondItem">
+            <FaTachometerAlt className='icon' />
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
+            <a href="#">Learn more</a>
+          </div>
+          <div id="thridItem">
+            <FaBriefcase className='icon' />
+            <h2>Lorem Ipsum</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec molestie velit libero, sit amet dignissim sem finibus a. Vivamus porttitor purus et mi suscipit, eget vehicula turpis molestie.</p>
+            <a href="#">Learn more</a>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div id="abrirModal" className="modal">
+          <div>
+          <a href="#fechar" title="Fechar" className="fechar">x</a>
+            <h2>Deseja trocar imagem do background?</h2>
+            <p>Seleciona uma imagem já existente ou faça upload de uma nova imagem</p>
+            <p>Você pode fazer qualquer coisa aqui, página de Login, pop-ups, ou formulários</p>
+          </div>
+        </div>
       </section>
     </div>
   );
